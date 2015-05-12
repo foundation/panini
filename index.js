e911b44a13ede519f112b3f1e9ada196e9583f5e
Geoff Kimball <geoff@zurb.com>
2015-05-12 14:45:50 -0700

    Add comments to the transform function to explain how it works

--- index.js
+++ index.js
@@ -41,22 +41,26 @@ module.exports = function(settings) {
   return through.obj(render);
 
   function render(file, enc, cb) {
+    // Get the HTML for the current page and layout
     var page = fm(file.contents.toString());
     var layout = page.attributes.layout || 'default';
     layout = fs.readFileSync(path.join(layouts, layout + '.html'));
 
+    // Now create Handlebars templates out of them
     var pageTemplate = Handlebars.compile(page.body + '\n');
     var layoutTemplate = Handlebars.compile(layout.toString());
 
+    // Next, extend the existing data object to include this page's metadata
     pageData = extend(pageData, page.attributes);
-
     pageData = extend(pageData, {
       page: path.basename(file.path, '.html')
     });
 
+    // Finally, add the page as a partial called "body", and render the layout template
     Handlebars.registerPartial('body', pageTemplate);
     file.contents = new Buffer(layoutTemplate(pageData));
 
+    // This sends the modified file back into the stream
     cb(null, file);
   }
 }
