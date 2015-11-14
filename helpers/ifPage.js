ecd75b102f60eb270862bc659eb32916398b417e
Geoff Kimball <geoff@zurb.com>
2015-11-13 20:39:19 -0800

    Add partials for ifpage and unlesspage

---
+++ helpers/ifPage.js
@@ -0,0 +1,15 @@
+module.exports = function(pageName) {
+  return function() {
+    var params = Array.prototype.slice.call(arguments);
+    var pages = params.slice(0, -1);
+    var options = params[params.length - 1];
+
+    for (var i in pages) {
+      if (pages[i] === pageName) {
+        return options.fn(this);
+      }
+    }
+
+    return '';
+  }
+}
