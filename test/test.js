c798b6e8e28dab960d4c4d0d976ef52ba3506fbc
Geoff Kimball <geoff@zurb.com>
2016-01-06 18:08:00 -0800

    Add tests for custom layouts

--- test/test.js
+++ test/test.js
@@ -1,23 +1,41 @@
 import { src, dest } from 'vinyl-fs';
-import panini from '../index';
 import assert from 'assert';
 import equal from 'assert-dir-equal';
+import { Panini } from '..';
 
 const FIXTURES = 'test/fixtures/';
 
 describe('Panini', function() {
   it('builds a page with a default layout', function(done) {
+    var p = new Panini({
+      root: FIXTURES + 'basic/pages/',
+      layouts: FIXTURES + 'basic/layouts'
+    });
+
+    p.refresh();
+
     src(FIXTURES + 'basic/pages/*')
-      .pipe(panini({
-        root: FIXTURES + 'basic/pages/',
-        layouts: FIXTURES + 'basic/layouts'
-      }))
+      .pipe(p.render())
       .pipe(dest(FIXTURES + 'basic/build'))
-      .on('data', data => {
-        console.log(data);
-      })
-      .on('end', () => {
+      .on('finish', () => {
         equal(FIXTURES + 'basic/expected', FIXTURES + 'basic/build');
+        done();
+      });
+  });
+
+  it('builds a page with a custom layout', function(done) {
+    var p = new Panini({
+      root: FIXTURES + 'layouts/pages/',
+      layouts: FIXTURES + 'layouts/layouts/'
+    });
+
+    p.refresh();
+
+    src(FIXTURES + 'layouts/pages/*')
+      .pipe(p.render())
+      .pipe(dest(FIXTURES + 'layouts/build'))
+      .on('finish', () => {
+        equal(FIXTURES + 'layouts/expected', FIXTURES + 'layouts/build');
         done();
       });
   });
