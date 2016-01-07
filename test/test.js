1565bf770e201d3311c0008a3dad385b04d3d0a8
Geoff Kimball <geoff@zurb.com>
2016-01-06 22:40:29 -0800

    Add unit tests for built-in helpers

--- test/test.js
+++ test/test.js
@@ -185,27 +185,111 @@ describe('Panini variables', function() {
 });
 
 describe('Panini helpers', function() {
-  xit('#code helper that renders code blocks', function(done) {
+  it('#code helper that renders code blocks', function(done) {
+    var p = new Panini({
+      root: FIXTURES + 'helper-code/pages/',
+      layouts: FIXTURES + 'helper-code/layouts/',
+    });
 
+    p.loadBuiltinHelpers();
+    p.refresh();
+
+    src(FIXTURES + 'helper-code/pages/**/*.html')
+      .pipe(p.render())
+      .pipe(dest(FIXTURES + 'helper-code/build'))
+      .on('finish', () => {
+        equal(FIXTURES + 'helper-code/expected', FIXTURES + 'helper-code/build');
+        done();
+      });
   });
 
-  xit('#ifEqual helper that compares two values', function(done) {
+  it('#ifEqual helper that compares two values', function(done) {
+    var p = new Panini({
+      root: FIXTURES + 'helper-ifequal/pages/',
+      layouts: FIXTURES + 'helper-ifequal/layouts/',
+    });
 
+    p.loadBuiltinHelpers();
+    p.refresh();
+
+    src(FIXTURES + 'helper-ifequal/pages/**/*.html')
+      .pipe(p.render())
+      .pipe(dest(FIXTURES + 'helper-ifequal/build'))
+      .on('finish', () => {
+        equal(FIXTURES + 'helper-ifequal/expected', FIXTURES + 'helper-ifequal/build');
+        done();
+      });
   });
 
-  xit('#ifPage helper that checks the current page', function(done) {
+  it('#ifpage helper that checks the current page', function(done) {
+    var p = new Panini({
+      root: FIXTURES + 'helper-ifpage/pages/',
+      layouts: FIXTURES + 'helper-ifpage/layouts/',
+    });
 
+    p.loadBuiltinHelpers();
+    p.refresh();
+
+    src(FIXTURES + 'helper-ifpage/pages/**/*.html')
+      .pipe(p.render())
+      .pipe(dest(FIXTURES + 'helper-ifpage/build'))
+      .on('finish', () => {
+        equal(FIXTURES + 'helper-ifpage/expected', FIXTURES + 'helper-ifpage/build');
+        done();
+      });
   });
 
-  xit('#markdown helper that converts Markdown to HTML', function(done) {
+  it('#markdown helper that converts Markdown to HTML', function(done) {
+    var p = new Panini({
+      root: FIXTURES + 'helper-markdown/pages/',
+      layouts: FIXTURES + 'helper-markdown/layouts/',
+    });
 
+    p.loadBuiltinHelpers();
+    p.refresh();
+
+    src(FIXTURES + 'helper-markdown/pages/**/*.html')
+      .pipe(p.render())
+      .pipe(dest(FIXTURES + 'helper-markdown/build'))
+      .on('finish', () => {
+        equal(FIXTURES + 'helper-markdown/expected', FIXTURES + 'helper-markdown/build');
+        done();
+      });
   });
 
-  xit('#repeat helper that prints content multiple times', function(done) {
+  it('#repeat helper that prints content multiple times', function(done) {
+    var p = new Panini({
+      root: FIXTURES + 'helper-repeat/pages/',
+      layouts: FIXTURES + 'helper-repeat/layouts/',
+    });
 
+    p.loadBuiltinHelpers();
+    p.refresh();
+
+    src(FIXTURES + 'helper-repeat/pages/**/*.html')
+      .pipe(p.render())
+      .pipe(dest(FIXTURES + 'helper-repeat/build'))
+      .on('finish', () => {
+        equal(FIXTURES + 'helper-repeat/expected', FIXTURES + 'helper-repeat/build');
+        done();
+      });
   });
 
-  xit('#unlessPage helper that checks the current page', function(done) {
+  it('#unlesspage helper that checks the current page', function(done) {
+    var p = new Panini({
+      root: FIXTURES + 'helper-unlesspage/pages/',
+      layouts: FIXTURES + 'helper-unlesspage/layouts/',
+    });
 
+    p.loadBuiltinHelpers();
+    p.refresh();
+
+    src(FIXTURES + 'helper-unlesspage/pages/**/*.html')
+      .pipe(p.render())
+      .pipe(dest(FIXTURES + 'helper-unlesspage/build'))
+      .on('finish', () => {
+        equal(FIXTURES + 'helper-unlesspage/expected', FIXTURES + 'helper-unlesspage/build');
+        done();
+      });
   });
 });
