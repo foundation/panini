5dbf8c3da2f51e7e845a02dbcb425907a82d7349
Geoff Kimball <geoff@geoffkimball.com>
2017-03-09 19:45:23 -0800

    Move smaller Panini class methods out of their own files and into the class definition

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ var EventEmitter = require('events').EventEmitter;
 var getConfig = require('flexiconfig');
 var handlebars = require('handlebars');
 var help = require('./lib/helpMessage');
+var loadData = require('./lib/loadData');
+var loadHelpers = require('./lib/loadHelpers');
+var loadLayouts = require('./lib/loadLayouts');
+var loadPartials = require('./lib/loadPartials');
 var inherits = require('util').inherits;
 var vfs = require('vinyl-fs');
+var path = require('path');
 
 /**
  * Initializes an instance of Panini.
@@ -36,10 +41,40 @@ function Panini(options) {
   }
 }
 
-Panini.prototype.refresh = require('./lib/refresh');
-Panini.prototype.loadBuiltinHelpers = require('./lib/loadBuiltinHelpers');
-Panini.prototype.render = require('./lib/render');
-Panini.prototype.getSourceStream = require('./lib/getSourceStream');
+Panini.prototype.refresh = function() {
+  var _this = this;
+  this.ready = false;
+
+  Promise.all([
+    loadLayouts(path.join(this.options.input, this.options.layouts), _this.Handlebars).then(function(layouts) {
+      _this.layouts = layouts;
+    }),
+    loadPartials(path.join(this.options.input, this.options.partials), _this.Handlebars),
+    loadHelpers(path.join(this.options.input, this.options.helpers), _this.Handlebars),
+    loadData(path.join(this.options.input, this.options.data)).then(function(data) {
+      _this.data = data;
+    })
+  ]).then(function() {
+    _this.ready = true;
+    _this.emit('ready');
+  });
+};
+
+Panini.prototype.loadBuiltinHelpers = function() {
+  this.Handlebars.registerHelper('ifequal', require('./helpers/ifEqual'));
+  this.Handlebars.registerHelper('markdown', require('./helpers/markdown'));
+  this.Handlebars.registerHelper('repeat', require('./helpers/repeat'));
+  this.Handlebars.registerHelper('code', require('./helpers/code'));
+};
+
+Panini.prototype.getSourceStream = function() {
+  return vfs.src(path.join(
+    this.options.input,
+    this.options.pages,
+    '**/*.*'
+  ));
+};
+
 Panini.prototype.onReady = function() {
   var _this = this;
 
@@ -51,7 +86,9 @@ Panini.prototype.onReady = function() {
       _this.once('ready', resolve);
     }
   });
-}
+};
+
+Panini.prototype.render = require('./lib/render');
 
 inherits(Panini, EventEmitter);
 
