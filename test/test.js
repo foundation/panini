805ba76e25400b79984079703ec323e083e0c7e9
Geoff Kimball <geoff@geoffkimball.com>
2017-04-12 21:03:50 -0700

    Move logic for end-to-end tests into one function to simplify things

--- test/test.js
+++ test/test.js
@@ -1,87 +1,53 @@
 'use strict';
 
-const {expect} = require('chai');
+const expect = require('chai').expect;
 const dest = require('vinyl-fs').dest;
-const equal = require('assert-dir-equal');
+const assertDirEqual = require('assert-dir-equal');
 const assign = require('lodash.assign');
 const panini = require('..');
 
-const FIXTURES = 'test/fixtures/';
-const p = (src, opts) => panini(src, assign({quiet: true}, opts), true);
+const fixtures = 'test/fixtures';
+const testFixture = (src, opts) => new Promise((resolve, reject) => {
+  panini(`${fixtures}/${src}`, assign({quiet: true}, opts), true)
+    .pipe(dest(`${fixtures}/${src}/build`))
+    .on('finish', () => {
+      assertDirEqual(`${fixtures}/${src}/build`, `${fixtures}/${src}/expected`);
+      resolve();
+    })
+    .on('error', reject);
+});
 
 describe('Panini', () => {
-  it('builds a page with a default layout', done => {
-    p(FIXTURES + 'basic')
-      .pipe(dest(FIXTURES + 'basic/build'))
-      .on('finish', () => {
-        equal(FIXTURES + 'basic/expected', FIXTURES + 'basic/build');
-        done();
-      })
-      .on('error', done);
+  it('builds a page with a default layout', () => {
+    return testFixture('basic');
   });
 
-  it('builds a page with an alternate layout', done => {
-    p(FIXTURES + 'layouts')
-      .pipe(dest(FIXTURES + 'layouts/build'))
-      .on('finish', () => {
-        equal(FIXTURES + 'layouts/expected', FIXTURES + 'layouts/build');
-        done();
-      })
-      .on('error', done);
+  it('builds a page with an alternate layout', () => {
+    return testFixture('layouts');
   });
 
-  it('builds a page with preset layouts by folder', done => {
-    p(FIXTURES + 'page-layouts', {
+  it('builds a page with preset layouts by folder', () => {
+    return testFixture('page-layouts', {
       pageLayouts: {
         alternate: 'alternate'
       }
-    })
-      .pipe(dest(FIXTURES + 'page-layouts/build'))
-      .on('finish', () => {
-        equal(FIXTURES + 'page-layouts/expected', FIXTURES + 'page-layouts/build');
-        done();
-      })
-      .on('error', done);
+    });
   });
 
-  it('builds a page with custom partials', done => {
-    p(FIXTURES + 'partials')
-      .pipe(dest(FIXTURES + 'partials/build'))
-      .on('finish', () => {
-        equal(FIXTURES + 'partials/expected', FIXTURES + 'partials/build');
-        done();
-      })
-      .on('error', done);
+  it('builds a page with custom partials', () => {
+    return testFixture('partials');
   });
 
-  it('builds a page with Front Matter properties added as variables', done => {
-    p(FIXTURES + 'front-matter')
-      .pipe(dest(FIXTURES + 'front-matter/build'))
-      .on('finish', () => {
-        equal(FIXTURES + 'front-matter/expected', FIXTURES + 'front-matter/build');
-        done();
-      })
-      .on('error', done);
+  it('builds a page with Front Matter properties added as variables', () => {
+    return testFixture('front-matter');
   });
 
-  it('builds a page with custom helpers', done => {
-    p(FIXTURES + 'helpers')
-      .pipe(dest(FIXTURES + 'helpers/build'))
-      .on('finish', () => {
-        equal(FIXTURES + 'helpers/expected', FIXTURES + 'helpers/build');
-        done();
-      })
-      .on('error', done);
+  it('builds a page with custom helpers', () => {
+    return testFixture('helpers');
   });
 
-  it('builds a page with external JSON data', done => {
-    p(FIXTURES + 'data')
-      .pipe(dest(FIXTURES + 'data/build'))
-      .on('finish', () => {
-        equal(FIXTURES + 'data/expected', FIXTURES + 'data/build');
-        done();
-      })
-      .on('error', done);
+  it('builds a page with external JSON data', () => {
+    return testFixture('data');
   });
 });
 
@@ -90,7 +56,7 @@ describe('Panini config', () => {
   const config = require('./fixtures/config/package.json');
 
   before(() => {
-    process.chdir(FIXTURES + 'config');
+    process.chdir(`${fixtures}/config`);
   });
 
   after(() => {
@@ -98,7 +64,7 @@ describe('Panini config', () => {
   });
 
   it('loads configuration from package.json', () => {
-    const stream = p('src');
+    const stream = panini('src', {quiet: true}, true);
     expect(stream._panini.options.pages).to.equal(config.panini.pages);
   });
 });
