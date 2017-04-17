e4c8b5930266a3fcf1280b61c7a102deeab4f6a9
Geoff Kimball <geoff@geoffkimball.com>
2017-04-17 11:34:03 -0700

    Use external dependency for root variable

--- test/utils.js
+++ test/utils.js
@@ -1,21 +1,7 @@
 'use strict';
 
-const path = require('path');
 const expect = require('chai').expect;
-const processRoot = require('../lib/process-root');
 const loadEngine = require('../lib/load-engine');
-
-describe('processRoot()', () => {
-  it('returns an empty string if both paths are on the same level', () => {
-    const pagePath = path.join(process.cwd(), 'src/pages/index.html');
-    expect(processRoot(pagePath, 'src/pages')).to.equal('');
-  });
-
-  it('returns a path prefix if paths are on different levels', () => {
-    const pagePath = path.join(process.cwd(), 'src/pages/about/index.html');
-    expect(processRoot(pagePath, 'src/pages')).to.equal('../');
-  });
-});
 
 describe('loadEngine()', () => {
   it('loads a built-in engine', () => {
