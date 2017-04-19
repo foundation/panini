e8d8bde305015ff1985c26f1cdc35e233dfc68e8
Geoff Kimball <geoff@geoffkimball.com>
2017-04-18 19:55:33 -0700

    Add proper file watching to CLI

--- bin/panini.js
+++ bin/panini.js
@@ -2,9 +2,12 @@
 
 'use strict';
 
+const path = require('path');
 const meow = require('meow');
 const vfs = require('vinyl-fs');
-const panini = require('..');
+const chalk = require('chalk');
+const watcher = require('glob-watcher');
+const Panini = require('..').Panini;
 
 const cli = meow(`
   Usage
@@ -25,4 +28,17 @@ if (cli.input.length < 2) {
   cli.showHelp(1);
 }
 
-panini(cli.input[0], {cli: true}).pipe(vfs.dest(cli.input[1]));
+const panini = new Panini({input: cli.input[0]});
+const pageRoot = path.join(process.cwd(), panini.options.input, panini.options.pages);
+const compile = () => panini.compile().pipe(vfs.dest(cli.input[1]));
+
+panini.refresh();
+compile();
+
+if (cli.flags.watch) {
+  watcher(path.join(pageRoot, '**/*.*'), {ignoreInitial: true}, () => {
+    return compile();
+  }).on('change', filePath => {
+    console.log(`\n${chalk.cyan('❯')} ${path.relative(pageRoot, filePath)} changed.\n`);
+  });
+}
