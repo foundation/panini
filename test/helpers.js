77354838fd1fa955a5a92324f56ae20023877f62
Geoff Kimball <geoff@geoffkimball.com>
2017-04-21 21:52:25 -0700

    Add currentPage() helper, an engine-agnostic version of the old ifPage helper

--- test/helpers.js
+++ test/helpers.js
@@ -3,6 +3,7 @@
 const assert = require('assert');
 const handlebars = require('handlebars');
 const repeat = require('../lib/repeat');
+const currentPage = require('../lib/current-page');
 
 describe('Helpers', () => {
   describe('repeat', () => {
@@ -22,6 +23,24 @@ describe('Helpers', () => {
         {{#repeat 3}}{{ @index }}{{/repeat}}
       `);
       assert.ok(t().indexOf('012'));
+    });
+  });
+
+  describe('currentPage', () => {
+    it('returns true if the page is within the given list', () => {
+      const h = handlebars.create();
+      const t = h.compile(`
+        {{#if (currentPage 'index' 'about')}}true{{/if}}
+      `);
+      assert.ok(t({currentPage: currentPage('index')}).indexOf('true'));
+    });
+
+    it('returns false if the page is not within the given list', () => {
+      const h = handlebars.create();
+      const t = h.compile(`
+        {{#if (currentPage 'index' 'about')}}true{{/if}}
+      `);
+      assert.ok(t({currentPage: currentPage('contact')}).indexOf('true') === -1);
     });
   });
 });
